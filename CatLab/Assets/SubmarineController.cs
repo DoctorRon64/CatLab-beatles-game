d0881122a436b9cd8401a093345efdab28ac24d5
DrRon <piepmail@lefthandmedia.com>
2024-01-26 14:58:58 +0100

    Submarine and vehicle class added

---
+++ CatLab/Assets/SubmarineController.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubmarineController : VehicleController
+{
+
+
+	private void Awake()
+	{
+		Health = MaxHealth;
+	}
+}
