77df46882d48775175906f937f3bc77533358af6
DoctorRon64 <piepmail@lefthandmedia.com>
2024-02-02 11:14:57 +0100

    audio manager correct

--- CatLab/Assets/Scripts/Manager/GameManager.cs
+++ CatLab/Assets/Scripts/Manager/GameManager.cs
@@ -12,24 +12,16 @@ public class GameManager : MonoBehaviour
 	[SerializeField] private AudioManager musicManager;
 	[SerializeField] private AudioManager SFXHitManager;
 	[SerializeField] private AudioManager SFXHealManager;
-	[SerializeField] private AudioManager beatManager;
     private bool theGameIsOverCheck = false;
 	private int beatValue = 0;
 
 	private void Awake()
 	{
 		deathPopUp.SetActive(false);
-		highscoreManager.HighScoreAction += OnBeatChange;
 		carController.onPlayerDied += GameOver;
         carController.onPlayerHit += PlayHitSFX;
 		carController.onPlayerHeal += PlayHealSFX;
     }
-
-	private void OnBeatChange(int _value)
-	{
-        beatValue = (beatValue == 1) ? 0 : 1;
-        beatManager.PlayAudio(beatValue);
-	}
 
 	private void PlayHitSFX()
 	{
